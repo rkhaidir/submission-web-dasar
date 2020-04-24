dc2d5a83ffd8c3ff702ee3ad8df6d4938947fc42
rkhaidir <rkhaidir085@gmail.com>
2020-04-24 09:39:18 +0800

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,20 @@ document.addEventListener("DOMContentLoaded", function(){
         nav.classList.toggle('show')
     })
 
+    const backTop = document.querySelector(".back-top")
 
-    const years = document.querySelector(".years")
-    let getYears = new Date()
-    years.innerHTML = getYears.getFullYear()
+    window.addEventListener("scroll", scrollUp)
+
+    function scrollUp() {
+        if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            backTop.style.display = ""
+        } else {
+            backTop.style.display = "none"
+        }
+    }
+
+    backTop.addEventListener("click", () => {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    })
 })
